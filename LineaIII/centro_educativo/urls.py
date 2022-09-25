8ccb5acf9a260b714ffbfa4538223df04d0bf5c8
Brian <brianfiquitiva12@gmail.com>
2022-09-25 17:35:40 -0500

    logon logic, and initial view of the home page

--- LineaIII/centro_educativo/urls.py
+++ LineaIII/centro_educativo/urls.py
@@ -3,11 +3,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth.views import LoginView, LogoutView
 
+from . import views
+
 app_name = "centro_educativo"
 urlpatterns = [
-    path("", LoginView.as_view(template_name='centro_educativo/login.html'), name="login")
+    path("", LoginView.as_view(template_name='centro_educativo/login.html'), name="login"),
+    path("home", views.home, name="home"),
+    path("logout", LogoutView.as_view(template_name='centro_educativo/login.html'), name="logout"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-"""path("users", views.users, name="users"),
+"""
     path("register", views.register, name="register"),
-    path("logout", LogoutView.as_view(template_name='app1/index.html'), name="logout"),
     path("modelbd", views.modelbd, name="modelbd"),"""
